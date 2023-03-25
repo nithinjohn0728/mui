b6325a90c6913ea25404f8058df0a8c9cdaf83e0
Nithin John Cherian <nithinjohn0728@gmail.com>
2023-03-25 09:59:31 +0000

    h4 as h1 and gutterbottom

--- src/components/Muitypography.tsx
+++ src/components/Muitypography.tsx
@@ -7,7 +7,7 @@ const Muitypography = () => {
     <Typography variant='h1'>this is H1</Typography>
     <Typography variant='h2'>this is H2</Typography> 
     <Typography variant='h3'>this is H3</Typography> 
-    <Typography variant='h4'>this is H4</Typography>
+    <Typography variant='h4' component = 'h1' gutterBottom>this is H4</Typography>
     <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facere optio expedita ea perspiciatis tenetur vero unde maiores quae temporibus dolore, nisi asperiores mollitia voluptatibus minus, corrupti qui! Laboriosam, nostrum!</Typography>
     <Typography variant='body2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem in labore maxime odio ex minus, perferendis culpa iste? Animi, adipisci eaque. Ex labore qui facere facilis perferendis adipisci in sit.</Typography>
     </div>
