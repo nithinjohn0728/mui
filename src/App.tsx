86388205f99363d9ac80d3fb01b033736e79bce5
Nithin John Cherian <nithinjohn0728@gmail.com>
2023-03-25 09:32:09 +0000

    created 4 headings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Muitypography from './components/Muitypography';
 
 function App() {
   return (
     <div className="App">
+      <Muitypography/>
       
     </div>
   );
---
+++ src/components/Muitypography.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const Muitypography = () => {
+  return (
+    <div>
+    <Typography variant='h1'>this is H1</Typography>
+    <Typography variant='h2'>this is H2</Typography> 
+    <Typography variant='h3'>this is H3</Typography> 
+    <Typography variant='h4'>this is H4</Typography>
+    </div>
+  )
+}
+
+export default Muitypography
