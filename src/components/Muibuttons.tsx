0b9473b87a1976305d4b302a160e7aca80042ac1
Nithin John Cherian <nithinjohn0728@gmail.com>
2023-03-25 11:25:23 +0000

    Added stack in place of div

--- src/components/Muibuttons.tsx
+++ src/components/Muibuttons.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Button } from '@mui/material'
+import { Button,Stack} from '@mui/material'
 
 const Muibuttons = () => {
+
+    //this is a comment
   return (
-    <div>
+    <Stack spacing={4}>
         <Button variant='text'>this is a text</Button>
         <Button variant='contained'>this is a contained button</Button>
         <Button variant='outlined'>this is a outlined button</Button>
-    </div>
+        
+    </Stack>
   )
 }
 
