942196bcb16794e7df40a615463e83aeba095b28
Nithin John Cherian <nithinjohn0728@gmail.com>
2023-03-25 13:08:44 +0000

    added different size buttons

--- src/components/Muibuttons.tsx
+++ src/components/Muibuttons.tsx
@@ -5,12 +5,29 @@ const Muibuttons = () => {
 
     //this is a comment
   return (
-    <Stack spacing={4}>
-        <Button variant='text'>this is a text</Button>
+    <div>
+    <Stack spacing={4} direction ='row'>
+        <Button variant='text' href = ''>this is a text</Button>
         <Button variant='contained'>this is a contained button</Button>
         <Button variant='outlined'>this is a outlined button</Button>
-        
     </Stack>
+
+    <Stack spacing={2} direction ='row'>
+        <Button variant='contained' color = 'primary'>primary</Button>
+        <Button variant='contained' color = 'secondary'>secondary</Button>
+        <Button variant='contained' color = 'info'>Info</Button>
+        <Button variant='contained' color = 'success'>Success</Button>
+    </Stack>
+
+
+
+    <Stack display='block' spacing = {2} direction = 'row' padding = {4}>
+        <Button variant='contained' size = 'small'>small button</Button>
+        <Button variant='contained' color = 'error' size = 'medium'>medium button</Button>
+        <Button variant='contained' color = 'warning' size = 'large'>large button</Button>
+
+    </Stack>
+    </div>
   )
 }
 
