b585ce77db8d76fd8b988cd09e635dd4ec54e03b
Nithin John Cherian <nithinjohn0728@gmail.com>
2023-03-25 11:19:48 +0000

    added three buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Muibuttons from './components/Muibuttons';
 import Muitypography from './components/Muitypography';
 
 function App() {
   return (
     <div className="App">
       <Muitypography/>
+      <Muibuttons />
       
     </div>
   );
---
+++ src/components/Muibuttons.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+const Muibuttons = () => {
+  return (
+    <div>
+        <Button variant='text'>this is a text</Button>
+        <Button variant='contained'>this is a contained button</Button>
+        <Button variant='outlined'>this is a outlined button</Button>
+    </div>
+  )
+}
+
+export default Muibuttons
